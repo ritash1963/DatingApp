08d34481a4c1ca81f3019c0f445b5e8941bb6dc7
ritash1963 <rita.shmidt@gmail.com>
2019-04-22 23:10:01 +0300

    end of section 8

--- Dating.API/Data/DataContext.cs
+++ Dating.API/Data/DataContext.cs
@@ -8,8 +8,8 @@ namespace Dating.API.Data
     {
          public DataContext(DbContextOptions<DataContext> options) : base (options) {} 
 
-         public DbSet<Value> Values { get; set;}
-         public DbSet<User> Users { get; set;}
-
+         public DbSet<Value> Values { get; set; }
+         public DbSet<User> Users { get; set; }
+         public DbSet<Photo> Photos { get; set; }
     }
 }
--- Dating.API/Startup.cs
+++ Dating.API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Dating.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -20,6 +21,7 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Dating.API.Helpers;
 
+
 namespace DatingApp.API
 {
     public class Startup
@@ -36,9 +38,15 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+             .AddJsonOptions(opt => {
+                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+             });
             services.AddCors();
+            services.AddAutoMapper();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                .AddJwtBearer(options => {
                    options.TokenValidationParameters = new TokenValidationParameters
@@ -53,7 +61,7 @@ namespace DatingApp.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -78,7 +86,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
-
+            //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
