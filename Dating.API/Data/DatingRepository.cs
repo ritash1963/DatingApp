3a901bd8b0d4f97f1dbbfc768103e4bf005b109a
ritash1963 <rita.shmidt@gmail.com>
2019-04-27 00:38:00 +0300

    end of section 14

--- Dating.API/Data/DatingRepository.cs
+++ Dating.API/Data/DatingRepository.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dating.API.Helpers;
 using Dating.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -40,11 +42,38 @@ namespace Dating.API.Data
             var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
             return user;
         }
-
-        public async Task<IEnumerable<User>> GetUsers()
+       public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
-            return users;
+             var users = _context.Users.Include(p => p.Photos)
+               .OrderByDescending(u => u.LastActive).AsQueryable();
+
+             users = users.Where(u => u.Id != userParams.UserId);
+
+             users = users.Where(u => u.Gender == userParams.Gender);
+
+            if (userParams.MinAge != 18 || userParams.MaxAge != 99)
+            {
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+                if (!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy)
+                {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
+            }
+
+                
+                users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
+            
+             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
